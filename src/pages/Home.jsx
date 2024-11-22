cfce8a1bdf66dac8f5e641e893d8a4bfb416fb3b
Nayan Jaiswal <nayan.jaiswal@coriolis.co.in>
2024-11-22 23:14:51 +0530

    chore: setup Redux Toolkit and configure store

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,32 @@
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { login, logout } from "../redux/slices/authSlice";
+
 const Home = () => {
-  return <div>HomePage</div>;
+  const dispatch = useDispatch(); // Dispatch actions
+  const user = useSelector((state) => state.auth.user);
+
+  const handleLogin = () => {
+    const fakeUser = { name: "Nayan" };
+    dispatch(login(fakeUser)); // Dispatch login action
+  };
+
+  const handleLogout = () => {
+    dispatch(logout()); // Dispatch logout action
+  };
+
+  return (
+    <div>
+      {user ? (
+        <>
+          <h1>Welcome, {user.name}</h1>
+          <button onClick={handleLogout}>Logout</button>
+        </>
+      ) : (
+        <button onClick={handleLogin}>Login</button>
+      )}
+    </div>
+  );
 };
 
 export default Home;
